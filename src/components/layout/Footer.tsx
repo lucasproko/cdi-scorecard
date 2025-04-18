eccf9798ca66a67f14e4e2c4e3bcc890b5dbd70a
lucasproko <lucaspro24+lucasprokogithub@gmail.com>
2025-04-18 09:40:14 -0700

    fix: removed broken links, changed name of app in footer and header

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,9 @@ export function Footer() {
   return (
     <footer className='bg-[#0B3D2E] text-white py-6'>
       <div className='container mx-auto px-4 text-center'>
-        <p className='text-sm'>Golf Tournament App &copy; {currentYear}</p>
+        <p className='text-sm'>
+          A tradition unlike any other &copy; {currentYear}
+        </p>
       </div>
     </footer>
   );
